5cb50f2372b56da8d0ba176c7d90682c48d625dd
unknown <omarelhanchi@gmail.com>
2024-03-03 21:31:05 +0100

    fundamentals commit

---
+++ Fundamentals/Fundamentals2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System ;
+using System.Runtime.CompilerServices;
+class Fundamentals
+{
+    static void Main ()
+    {
+        // Print all of the integers from 1 to 255.
+            for (int i=1 ; i <=255 ; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+        // Print all of the integers from 1 to 255.
+            for (int i=1 ; i <=255 ; i++)
+            {
+                if (i % 2 != 2) 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        // print sum of all numbers from 0 to 255 
+            int sum = 0;
+            for (int i=1 ; i<=255 ; i++ )
+            {
+                sum += i ;
+                Console.WriteLine($"new number : {i}  , sum is  {sum} ");
+            }
+    }
+}
+
---
+++ Fundamentals/Fundamentals_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    { int i = 1 ; 
+    while (i<= 255) {
+        Console.WriteLine(i);
+        i++ ; 
+    }
+    {
+        
+    }
+     
+    /*
+       // Use a for loop to iterate from 1 to 255
+        for (int i = 1; i <= 255; i++)
+        {
+            Console.WriteLine(i);
+        }
+    */
+    Console.WriteLine("...........................................");
+    for (int val = 1 ; val <= 100 ; val++)
+    {
+    if (val % 15 ==0) {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if  (val % 3 == 0) {
+        Console.WriteLine("Fizz");
+    }
+    else if  (val % 5 == 0) {
+        Console.WriteLine("Buzz");
+    }
+    }
+
+    } 
+}
+
