0f0e7f1a6d487c57818dcb736a0a046d3038ba57
unknown <omarelhanchi@gmail.com>
2024-04-08 18:14:04 +0100

    dojo survey with model assignement first commit

---
+++ Lectures code/DojoSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+        [HttpPost("register")]
+        public IActionResult Register(Survey ninja)
+        {
+     
+        return RedirectToAction("DisplayResults", ninja); 
+        }
+
+        public IActionResult DisplayResults(Survey ninja)
+        {
+        return View("Results",ninja); 
+        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Lectures code/DojoSurveyWithModel/Models/Survey.cs
@@ -0,0 +1,9 @@
+#pragma warning disable CS8618
+namespace DojoSurveyWithModel.Models;
+public class Survey 
+{
+    public string Name {get;set;}
+    public String Location {get;set;}
+    public String FavouriteLanguage {get;set;}
+    public string Comment {get;set;}
+}
